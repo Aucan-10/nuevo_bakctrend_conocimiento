8f968a0813160d12a122469ddc36dedba43b2c66
Aucan <aucannrusso2007@gmail.com>
2026-07-01 11:37:27 -0300

    V10 - Fixed Errors in vercel.json

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
-// //MODO CORRER EN PC
-// import app from "./app.js";
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`Corriendo app en ${PORT}`);
-// });
-
-//==== MODO SEVERLESS ====
 import app from "./app.js";
 
-// Exportar para Vercel (Serverless)
+// Exportar para Vercel
 export default app;
 
-// Solo escuchar puerto si NO estamos en Vercel (desarrollo local)
+// Solo escuchar puerto en desarrollo local
 if (process.env.NODE_ENV !== "production" && !process.env.VERCEL) {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
