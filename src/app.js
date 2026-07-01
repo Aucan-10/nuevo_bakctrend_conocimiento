0ce08ed377ec7dab50087413294939138e78f83c
Aucan <aucannrusso2007@gmail.com>
2026-07-01 12:04:13 -0300

    V12 - Deploy updates

--- src/app.js
+++ src/app.js
@@ -5,9 +5,15 @@ import userRoutes from "./routes/user.routes.js";
 import subjectRoutes from "./routes/subject.routes.js";
 import enrollmentRoutes from "./routes/enrollment.routes.js"; // ← AGREGAR
 import healthRoutes from "./routes/health.routes.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app = express();
+// Cargar la documentación de Swagger
+const swaggerDocument = YAML.load("./swagger.yaml");
 
+// Configurar Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
